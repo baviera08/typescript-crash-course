27f772480c8c4f68a7f258c5860d0b36555fd2c1
Matias Bavera <matiasbavera@gmail.com>
2020-07-02 22:44:42 -0400

    Added interfaces examples

---
+++ 04-objects-interfaces.ts
@@ -0,0 +1,31 @@
+// Inferred Object interfaces
+
+let robot = {
+    name: 'wallie',
+    color: 'gray'
+}
+
+// Describe objects using {}  and property names
+
+let developer: {
+    name: string,
+    age: number,
+    programingLanguage?: string
+}
+
+developer = {
+    name: 'John',
+    age: 22,
+    // hue:false // (should give an error)
+}
+
+// Describe objects using interfaces
+interface IRobot {
+    name: string;
+    color: string;
+}
+
+let cleaner: IRobot = {
+    name: 'cleaner',
+    color: 'gray'
+}
