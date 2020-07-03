0ce55ae09d8177d57d169753c6a55187a0686871
Matias Bavera <matiasbavera@gmail.com>
2020-07-02 22:27:13 -0400

    Added basic-types and functions

--- 02-basic-types/basic-types.ts
+++ 02-basic-types/basic-types.ts
@@ -1,7 +1,7 @@
 // Uninitialized variables typed via type annotations.
 let robotName: string;
 let redirect: boolean;
-let robotId: number; 
+let robotId: number;
 
 // Uninitialized variables are type `any` by default (Not recommended)
 let robot1;
@@ -14,15 +14,15 @@ let isRobotAvailable = true;
 let robotWheel = 4;
 
 // Arrays
-let robots=['robot1', 'robot2'];
+let robots = ['robot1', 'robot2'];
 let fleets: string[] = [];
 
 // Tuples
-let robotAge:[string, number];
-let robotAges:[string,number][] = [];
+let robotAge: [string, number];
+let robotAges: [string, number][] = [];
 
 // Union Types
-let stringNull: string| null;
+let stringNull: string | null;
 let stringUndefined: string | undefined;
 
 // Enums, zero based integer values by default
@@ -31,7 +31,7 @@ enum RobotType {
     Patroller
 }
 let patroller = RobotType.Patroller;
-let robotType : RobotType;
+let robotType: RobotType;
 let cleanerIndex = RobotType.Cleaner.valueOf;
 
 // Enums, with values set explicitly
@@ -41,29 +41,39 @@ enum RobotMovement {
     stop = 'stop-movement'
 }
 let right = RobotMovement.right;
-let robotMovement:RobotMovement;
+let robotMovement: RobotMovement;
 let stopKey = RobotMovement.stop.valueOf();
 
 // Type alias. Using string literal types
 type RobotMovementType = 'right-movement' | 'stop-movement';
 
+let a: RobotMovementType = 'right-movement'
 // Example
 // let r:RobotMovementType = '';
 // let i = RobotMovement.
 
 // Enums are not type safe
 enum Num {
-    zero=0,
-    one=1,
-    two=2,
+    zero = 0,
+    one = 1,
+    two = 2,
 }
-let num:Num = 3;
+let num: Num = 3;
 
 /**
  * Type assertions can be useful when working with `any` or `unknown` types
  * - This is useful when interacting with browser/vendor APIs
  */
 
-let any:any ='hi';
+let any: any = 'hi';
+// I saying here I know what I'm doing and I'm casting the any type to string.
 let thisIsAString = <string>any;
 let thisIsAString2 = any as string;
+
+
+// Other types
+let varNull: null;
+let varUndefined: undefined;
+let varVoid: undefined;
+let varAny: any;
+let varUnknown: unknown;
--- 02-basic-types/functions.ts
+++ 02-basic-types/functions.ts
@@ -1,6 +1,24 @@
 // Return types of functions can be inferred
-let getRandomNumber = () => Math.random() >0.5;
+let getRandomNumber = () => Math.random() > 0.5;
 let bark = () => 'guau';
-let getObject = () => {};
+let getObject = () => { };
 
 // Type annotations can be used to type uninitialized functions
+let sendMessage: (message: string) => void;
+let getNote: (id: number) => string;
+let isItTrue: () => true;
+
+// You can add explicit return type annotations when a function is initialized
+// * This ensures the function returns the right type
+let sayHello = (message = 'Hello'): string => {
+    return message
+}
+
+// Optional & rest parameters
+
+// Default value for the parameter
+let greet = (message = 'hi') => console.log(message);
+// Optional parameter (?? nullish coalescing operator. Handle JUST the null | undefined). https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing_operator
+let maybeGreet = (message?: string) => console.log(message ?? 'hello');
+// Rest parameter ...
+let join = (separator: string, ...strings: string[]) => strings.join(separator);
